eda05fb4d5cd88f4a0a3c7329fae9b335c509d49
TalaMarn <talamarn270@gmail.com>
2026-03-14 15:48:39 +0630

    modified:   week 7/myproject/db.sqlite3 	new file:   week 7/myproject/students/__pycache__/forms.cpython-314.pyc 	modified:   week 7/myproject/students/__pycache__/urls.cpython-314.pyc 	modified:   week 7/myproject/students/__pycache__/views.cpython-314.pyc 	new file:   week 7/myproject/students/forms.py 	new file:   week 7/myproject/students/templates/add_student.html 	new file:   week 7/myproject/students/templates/dashboard.html 	modified:   week 7/myproject/students/urls.py 	modified:   week 7/myproject/students/views.py

--- week 7/myproject/students/urls.py
+++ week 7/myproject/students/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.student_list, name='student_list'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add/', views.add_student, name='add_student'),
 ]
--- week 7/myproject/students/views.py
+++ week 7/myproject/students/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Student
 from django.core.paginator import Paginator
+from .forms import StudentForm
 
 # Create your views here.
 def student_list(request):
@@ -14,3 +15,22 @@ def student_list(request):
     page_number = request.GET.get('page')
     students = paginator.get_page(page_number)
     return render(request, 'student.html', {'students': students})
+
+def dashboard(request):
+    total_students = Student.objects.count()
+
+    context = {
+        'total_students': total_students,
+    }
+    return render(request, 'dashboard.html', context)
+
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = StudentForm()
+
+    return render(request, 'add_student.html', {'form': form})
